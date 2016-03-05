107a0c5cbf791454d17310a28462b4a4a3ad4809
David Cramb <david.cramb@gmail.com>
2016-03-05 10:19:28 -0600

    did the first batch of exercise of hiding stuff

---
+++ music.js
@@ -0,0 +1,12 @@
+var addMusicView = document.getElementById('AMV');
+//Hides the addMusicView on pageload
+function hideBox (div) {
+  div.hidden = true;
+}; hideBox(addMusicView);
+//
+
+function addViewButtons (navLinks) {
+  for (var i = 0; i < navLinks.ul.li.length; i++);
+    console.log('hi');
+};
+addViewButtons(document.querySelector('nav'))
