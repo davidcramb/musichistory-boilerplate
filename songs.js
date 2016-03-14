1d9b7bfe6c417dfb7fe25b5080cb07083d5e9735
David Cramb <david.cramb@gmail.com>
2016-03-14 16:43:17 -0500

    put navbar inside container div and regex works on array now

--- songs.js
+++ songs.js
@@ -5,3 +5,24 @@ songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast 
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+function addSongtoEnd (songarray, newsong) {
+  songarray.push(newsong)
+};
+
+function addSongtoBeginning (songarray, newsong){
+  songarray.unshift(newsong)
+}
+addSongtoBeginning (songs, "My Humps by The Black Eye Peas");
+
+addSongtoEnd(songs, "Under the Bridge by Red Hot Chili Peppers");
+
+function removeCrap (crap) {
+  console.log()
+   for (var i = 0; i < crap.length; i ++){
+    var toString = crap[i].replace(/[!@##$%^&*()><]/g,"")
+    console.log(toString)
+
+  }
+};
+removeCrap(songs)
