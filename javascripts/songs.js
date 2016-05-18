9b8c3623a6378856098ac2f4ef31688902ff43db
David Cramb <david.cramb@gmail.com>
2016-05-18 18:01:20 -0500

    Added edit function Delete function not operational currently due to change in how we grab for firebase ID. Next fix.

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -9,9 +9,9 @@ hideAddMusicView();
 function addSongsToDOM (songs) {
   for (let song in songs) {
    let currentSong = songs[song];
-    $('#songInfoBox').append(`<div class='musicRow' id='${song}'>
-                              <h2 class="songName"><span class="btn delete">X</span>${currentSong.song}</h2>
-                              <div class="songData">${currentSong.song} by ${currentSong.artist} on the album ${currentSong.album}
+    $('#songInfoBox').append(`<div class='musicRow' id='del_${song}'>
+                              <h2 class="songName"><span class="btn delete">X</span>${currentSong.title}</h2>
+                              <div class="songData">${currentSong.title} by ${currentSong.artist} on the album ${currentSong.album}<span class="btn edit" id="edit_${song}">edit</span>
                               </div>`);  }
 }
 
@@ -21,25 +21,26 @@ $.ajax({
   type: 'GET'
   }).done(function(songs){
   addSongsToDOM(songs);
-  songList.push(songs);
+  songList.songs = songs;
   });
+  // if ($('#add').attr('hidden'))
+  console.log($('#add:hidden'));
+  if ($('#add:hidden')) {
+    $('#add').show();
+  }
 };
 
-// var getSong = () => {
-//   $.ajax({
-//     url: `https://dcc-music-history.firebaseio.com/music/${songID}.json`
-//   }).done(function(songID){
-//     console.log(songID)
-//   });
-// };
-  
+let getFirebaseID = (id) => {
+  console.log(id)
+  return id.split("edit_")[1];
+}
+
 $('#add').click(function () {
   let newSong = {
-    "song": $('#addSong').val(),
+    "title": $('#addSong').val(),
     "artist": $('#addArtist').val(),
     "album": $('#addAlbum').val()
     };
-
   $.ajax({
     url: 'https://dcc-music-history.firebaseio.com/music/.json',
     type: 'POST',
@@ -55,6 +56,8 @@ $('#addMusic').click(function() {
   $('#addMusicView').fadeIn();
 });
 $('#listMusic').click(function(){
+  $('#songInfoBox').empty();
+  pageLoad();
   $('#addMusicView').fadeOut();
   $('#listMusicView').fadeIn();
 });
@@ -62,20 +65,72 @@ $('#listMusic').click(function(){
 $(document).on('click', '.delete', function() {
   let objecttoDelete = $(this).parent().parent();
   objecttoDelete.remove();
+  // console.log(objecttoDelete.get(0).id);
+  getFirebaseID(objecttoDelete.get(0).id);
 $.ajax({
-    url: `https://dcc-music-history.firebaseio.com/music/${objecttoDelete.get(0).id}.json`,
+    url: `https://dcc-music-history.firebaseio.com/music/${getFirebaseID(objecttoDelete.get(0).id)}.json`,
     type: 'DELETE'
   });
 });
 
-pageLoad();
+//TO DO
+$(document).on('click', '.edit', function() {
+  $('#listMusicView').fadeOut();
+  $('#addMusicView').fadeIn()
+  $('#addMusicView button').hide();
+  
+  $('<button id="edit-current-song" class="btn">Edit</button>').appendTo("#addMusicView");
+  // $('#addMusicView button').text('Edit').removeAttr('id').addClass('edit-current-song');
+
+  let songID = getFirebaseID($(this).get(0).id);
+  console.log(songID)
+  $.ajax({
+    url: `https://dcc-music-history.firebaseio.com/music/${songID}.json`,
+    type: 'GET'
+  }).done(function(song){
+    console.log(song)
+    let songEditObject = { };
+    $(document).on('click', '#edit-current-song', function() {
+      songEditObject.title = $('#addSong').val();
+      songEditObject.artist = $('#addArtist').val();
+      songEditObject.album = $('#addAlbum').val();
+
+      console.log(songEditObject)
+      // console.log(songEditObject);
+      $.ajax({
+        url: `https://dcc-music-history.firebaseio.com/music/${songID}.json`,
+        type: 'PUT',
+        data: JSON.stringify(songEditObject),
+      }).done(function(barg) {
+        $('#songInfoBox').empty();
+        pageLoad();
+
+        $('#songInfoBox').empty();
+        
+        $('#listMusicView').fadeIn();
+        
+        $('#edit-current-song').remove();
+
+      })
+    });
+  });
 
 });
 
-let songList = [];
+//ON PAGE LOAD
+pageLoad();
+});
 
-let filter = () => {
-  console.log(songList)
-  let testsearchByArtist = "Pink Floyd";
-  console.log(testsearchByArtist);
-}
+let songList = { songs: {}}
+
+// let filter = (artist) => {
+//   console.log(songList)
+//   let testsearchByArtist = "Pink Floyd";
+//   for (let key in songList.songs) {
+//     var currentSong = songList.songs[key];
+//     if (currentSong.artist === testsearchByArtist) {
+//       console.log(currentSong)
+//     }
+//   }
+// }
+
